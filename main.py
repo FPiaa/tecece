8eb97046abd6f27e5109598ac2fb2bf326c4a37b
Igor Fidler <igorgfidler@hotmail.com>
2025-02-12 13:58:00 -0300

    Adicionar inicializador do projeto

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import shutil
 import os
 import pathlib
 import subprocess
+import json
 
 def convert(filename, file_dest):
     with open(filename, "r") as f:
@@ -80,21 +81,56 @@ def exec_cmd(args):
     
     subprocess.call(["python3",  args.entrypoint])
 
+def init_project(args):
+    src_path = args.input_dir or "./src"
+    main_file = os.path.join(src_path, args.entrypoint or "main.py")
+    
+    if os.path.exists("./maspy-dsl.json") and os.path.exists(src_path):
+        print("Projeto já inicializado")
+        return
+
+    if not os.path.exists(src_path):
+        os.makedirs(src_path)
+        print(f"Diretório criado: {src_path}")
+    
+    if not os.path.exists(main_file):
+        with open(main_file, "w") as f:
+            f.write("from maspy import *\n")
+        print(f"Arquivo criado: {main_file}")
+
+    if not os.path.exists('./maspy-dsl.json'):
+        base_args = {
+            "input-dir": "./src",
+            "output-dir": "./build",
+            "entrypoint": "main.py"
+        }
+        with open('./maspy-dsl.json', 'w') as w:
+            print(json.dumps(base_args))
+            w.write(json.dumps(base_args, indent=4))
+
+
+
 def main():
     parser = argparse.ArgumentParser(description="MASPY DSL")
 
     # Adiciona os subcomandos
     subparsers = parser.add_subparsers(dest='comando', help='Subcomandos disponíveis')
 
+    # Subcomando init
+    parser_init = subparsers.add_parser('init', help="Inicializa um novo projeto MASPY-DSL")
+    parser_init.add_argument('-i', '--input-dir', type=str, help='Diretório contendo o código do projeto. Diretório padrão "./src"')
+    parser_init.add_argument('-s', '--entrypoint', type=str, help='Ponto de entrada para execução. Entrada padrão "./src/main.py"')
+
     # Subcomando 'build'
     parser_build = subparsers.add_parser('build', help='Executa a conversão das anotações da DSL para código python válido')
-    parser_build.add_argument('-o', '--output-dir', type=str, help='Diretório destino das transformações. Diretório padrão "./build"', default='./build')
-    parser_build.add_argument('-i', '--input-dir', type=str, help='Diretório contendo o código do projeto. Diretório padrão "."', default="./src")
+    parser_build.add_argument('-o', '--output-dir', type=str, help='Diretório destino das transformações. Diretório padrão "./build"')
+    parser_build.add_argument('-i', '--input-dir', type=str, help='Diretório contendo o código do projeto. Diretório padrão "./src"')
     parser_build.add_argument('-v', '--verbose', action='store_true', help='Exibe todos os detalhes da conversão', default=False)
 
     # Subcomando 'exec'
     parser_exec = subparsers.add_parser('exec', help='Executa o projeto da DSL transformado em python válido')
-    parser_exec.add_argument('-s', '--entrypoint', type=str, help='Ponto de entrada para execução. Entrada padrão "./build/main.py"', default="./build/main.py")
+    parser_exec.add_argument('-s', '--entrypoint', type=str, help='Ponto de entrada para execução. Entrada padrão "./build/main.py"')
+
 
     # Verifica se o comando foi passado
     if len(sys.argv) == 1:
@@ -110,6 +146,8 @@ def main():
         build(args)
     elif args.comando == 'exec':
         exec_cmd(args)
+    elif args.comando == 'init':
+        init_project(args)
     else:
         parser.print_help()
 
