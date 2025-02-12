050929f07d4df1ecbd19b749a02628138faf22e4
Igor Fidler <igorgfidler@hotmail.com>
2025-02-12 12:35:41 -0300

    preparar main para integrar com a cli

--- main.py
+++ main.py
@@ -2,11 +2,12 @@ import ast
 import astor
 from dsl.PythonTransformer import PythonTransformer
 
-with open("example.py", "r") as f:
-    tree = ast.parse(f.read())
-    tree = PythonTransformer().visit(tree)
-    tree = ast.fix_missing_locations(tree)
-    with open("build.py", "w") as f:
-        f.write(astor.to_source(tree))
-    # exec(compile(tree, "<ast>", mode="exec"))
-    print()
+def convert(filename, file_dest):
+    with open(filename, "r") as f:
+        tree = ast.parse(f.read())
+        tree = PythonTransformer().visit(tree)
+        tree = ast.fix_missing_locations(tree)
+        with open("build.py", "w") as w:
+            w.write(astor.to_source(tree))
+
+convert("./example.py", None)
