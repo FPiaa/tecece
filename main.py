a3107d4212384d9a12839f1ba0a5a954e5bcae02
Igor Fidler <igorgfidler@hotmail.com>
2025-02-12 00:07:51 -0300

    Integrar todas as transformações

--- main.py
+++ main.py
@@ -32,34 +32,26 @@ from ast import (
 from dsl import dsl
 
 class DslToPython(ast.NodeTransformer):
+    ctx_functions = []
 
+    def visit_ClassDef(self, node: ast.ClassDef) -> Any:
+        self.generic_visit(node)
+        node.body = [*self.ctx_functions, *node.body]
+        return node
+    
     def visit_FunctionDef(self, node: ast.FunctionDef) -> Any:
         precondition_found = [
             x
             for x in node.decorator_list
             if isinstance(x, ast.Call)
             and (
-                (isinstance(x.func, ast.Name) and x.func.id == "precondition")
-                or (isinstance(x.func, ast.Attribute) and x.func.attr == "precondition")
+                (isinstance(x.func, ast.Name) and x.func.id == "pl")
+                or (isinstance(x.func, ast.Attribute) and x.func.attr == "pl")
             )
         ]
 
         if not precondition_found:
             return node
-        node.decorator_list = [
-            x
-            for x in node.decorator_list
-            if not (
-                isinstance(x, ast.Call)
-                and (
-                    (isinstance(x.func, ast.Name) and x.func.id == "precondition")
-                    or (
-                        isinstance(x.func, ast.Attribute)
-                        and x.func.attr == "precondition"
-                    )
-                )
-            )
-        ]
 
         if not isinstance(precondition_found[0].args[0], ast.Constant):
             return node
@@ -69,7 +61,13 @@ class DslToPython(ast.NodeTransformer):
         # early return
         
         print(astor.to_source(precondition[2]))
-        # print(ast.dump(precondition[2], indent=4))
+        for x in node.decorator_list:
+            print("in decorator")
+            if isinstance(x, ast.Call) and (isinstance(x.func, ast.Name) and x.func.id == 'pl' or isinstance(x.func, ast.Attribute) and x.func.attr == 'pl'):
+                call = ast.Name(id=precondition[2].name, ctx=ast.Load())
+                x.args = [precondition[0], precondition[1], call]
+                self.ctx_functions.append(precondition[2])
+        print(ast.dump(node, indent=4))
         return node
 
 
@@ -79,5 +77,5 @@ with open("example.py", "r") as f:
     tree = ast.fix_missing_locations(tree)
     with open("build.py", "w") as f:
         f.write(astor.to_source(tree))
-    exec(compile(tree, "<ast>", mode="exec"))
+    # exec(compile(tree, "<ast>", mode="exec"))
     print()
